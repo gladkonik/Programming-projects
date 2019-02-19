6b588b8437bee18885abe8c8f5d2eeb545fef94a
nikin <gladkonik@gmail.com>
2019-02-20 00:39:24 +0300

    A bug fix + beautification

--- Test_Task_Confirmit/Program.cs
+++ Test_Task_Confirmit/Program.cs
@@ -4,71 +4,21 @@ using System.Linq;
 
 namespace Test_Task_Confirmit
 {
-    public class TextFormatter
-    {
-        public string Justify(string text, int width)
-        {
-            char[] separators = { ' ' };
-            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-            string result = "";
-            int pos = 0;
-            int pos_prev = 0;
-            while (true) {
-                int len = 0; //length of current row
-                do {
-                    len += words[pos++].Length;
-                    len++; //space
-                } while (pos < words.Length && len + words[pos].Length < width);
-
-                len--; //take away the last space 
-                if (pos == words.Length) {
-                    for (int j = pos_prev; j < words.Length; j++) {
-                        result += words[j];
-                        result += (words[j] == words[words.Length - 1]) ? '\n' : ' ';
-                    }
-                    return result;
-                }
-                int n_words = pos - pos_prev;
-                int n_spaces = n_words - 1 + width - len; // cumulative number of spaces for current string
-                for (int j = 0; j < n_words; j++) {
-                    result += words[pos_prev + j];
-                    if (j + 1 < n_words) {
-                        result += string.Concat(Enumerable.Repeat(" ", n_spaces / (n_words - 1)));
-                        if (j < n_spaces % (n_words - 1)) {
-                            result += ' ';
-                        }
-                    }
-                    else {
-                        result += '\n';
-                        int lenght = result.Length;
-                    }
-                }
-                pos_prev = pos;
-            }
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
             string path = @"../input.txt";
 
-            // Delete the file if it doesn't exist.
             if (!File.Exists(path)) {
                 System.Console.WriteLine("Input file not found.\n");
                 return;
             }
+
             string text = System.IO.File.ReadAllText(path);
-            //System.Console.WriteLine(text);
-            //            System.Console.WriteLine(@"You can put a backslash \ here
-            //and a new line
-            //and tabs			work too. 
-            //You can also put in sequences that would normally be seen as escape sequences \n \t");
-            //        }
             TextFormatter format = new TextFormatter();
             path = @"../output.txt";
             System.IO.File.WriteAllText(path, format.Justify(text, 30));
-
         }
     }
 }
---
+++ Test_Task_Confirmit/TextFormatter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace Test_Task_Confirmit
+{
+    public class TextFormatter
+    {
+        private string result;
+        private string[] words;
+
+        public string Justify(string text, int width)
+        {
+            words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            result = string.Empty;
+            var pos = 0;
+            var pos_prev = 0;
+
+            while (pos < words.Length)
+            {
+                var rowLength = 0; //length of current row
+
+                do
+                {
+                    rowLength += words[pos++].Length + 1; // plus space
+                } while (pos < words.Length && rowLength + words[pos].Length <= width);
+
+                rowLength--; //take away the last space
+
+                if (pos != words.Length)
+                {
+                    addLine(pos, pos_prev, width, rowLength);
+                    pos_prev = pos;
+                }
+            }
+
+            for (int j = pos_prev; j < words.Length; j++) // adding the last line
+            {
+                result += words[j];
+                result += (words[j] != words[words.Length - 1]) ? " " : string.Empty;
+            }
+            return result;
+        }
+
+        private void addLine(int pos, int pos_prev, int width, int rowLength)
+        {
+            var wordsCount = pos - pos_prev;
+            var spacesInRowCount = wordsCount - 1;
+            var singleSpacesCount = spacesInRowCount + width - rowLength; // cumulative number of spaces for current string
+
+            for (var j = 0; j < wordsCount; j++)
+            {
+                result += words[pos_prev + j];
+
+                if (j < spacesInRowCount)
+                {
+                    result += string.Concat(Enumerable.Repeat(" ", singleSpacesCount / spacesInRowCount));
+
+                    if (j < singleSpacesCount % spacesInRowCount)
+                    {
+                        result += ' ';
+                    }
+                }
+            }
+            result += '\n';
+        }
+    }
+}
