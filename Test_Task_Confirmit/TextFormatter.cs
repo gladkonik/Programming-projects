6bfb930263afad855be56a17cd18425d1eb0388e
nikin <gladkonik@gmail.com>
2019-02-20 18:49:50 +0300

    Glossed

--- Test_Task_Confirmit/TextFormatter.cs
+++ Test_Task_Confirmit/TextFormatter.cs
@@ -17,22 +17,23 @@ namespace Test_Task_Confirmit
 
             while (pos < words.Length)
             {
-                var rowLength = 0; //length of current row
+                var rowLength = 0; // length of current row
 
                 do
                 {
                     rowLength += words[pos++].Length + 1; // plus space
                 } while (pos < words.Length && rowLength + words[pos].Length <= width);
 
-                rowLength--; //take away the last space
+                rowLength--; // take away the last space
 
                 if (pos != words.Length)
                 {
-                    addLine(pos, pos_prev, width, rowLength);
+                    AddLine(pos, pos_prev, width, rowLength);
                     pos_prev = pos;
                 }
             }
 
+            // adding the last line
             for (int j = pos_prev; j < words.Length; j++) // adding the last line
             {
                 result += words[j];
@@ -41,7 +42,7 @@ namespace Test_Task_Confirmit
             return result;
         }
 
-        private void addLine(int pos, int pos_prev, int width, int rowLength)
+        private void AddLine(int pos, int pos_prev, int width, int rowLength)
         {
             var wordsCount = pos - pos_prev;
             var spacesInRowCount = wordsCount - 1;
